26c8ad55f71bcc8dd184da2b43b8e96005d95e38
Michael Szostak <mszostak@mymail.mines.edu>
2016-09-27 11:55:34 -0600

    Changed the mouth to red and the eyes are higher and to the right

--- src/Cat.java
+++ src/Cat.java
@@ -7,14 +7,14 @@ public class Cat {
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 300;
 	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/2;
-	private static final int EYE_X = HEAD_DIMENSION/3;
+	private static final int EYE_Y = (HEAD_DIMENSION - 50)/2;
+	private static final int EYE_X = (HEAD_DIMENSION + 50)/3;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = HEAD_DIMENSION/10;
 	private static final int EYE_WIDTH = HEAD_DIMENSION/10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
+	private static final int MOUTH_HEIGHT = 17;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
@@ -37,7 +37,7 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.red);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
