d25688b9e301aa106237f195378df32d39a258aa
Hunter Arnell <harnell@adit.mines.edu>
2016-09-27 12:13:47 -0600

    Made cat shape oval, overhaul of text

--- src/Cat.java
+++ src/Cat.java
@@ -5,7 +5,7 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 750;
+	private static final int HEAD_DIMENSION = 500;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = (HEAD_DIMENSION - 50)/2;
 	private static final int EYE_X = (HEAD_DIMENSION + 50)/3;
@@ -28,7 +28,7 @@ public class Cat {
 		int y=catY;
 		// Draw the head
 		g2.setColor(Color.black);
-		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
+		g2.fillOval(x, y, HEAD_DIMENSION, (int)(1.5*HEAD_DIMENSION));
 		// Draw the eyes
 		g2.setColor(Color.red);
 		x = catX + EYE_X; 
@@ -37,13 +37,13 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.red);
+		g2.setColor(Color.white);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
-		g2.setColor(Color.white);
+		g2.setColor(Color.blue);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", catX, catY+(int)(1.5*HEAD_DIMENSION)+10);	
 	}
 }
